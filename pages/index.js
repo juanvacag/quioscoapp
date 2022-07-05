fbd4b2436ec0eb5569aa3f3baf6cd3d5f2daa645
juanvacag <>
2022-07-05 17:46:20 +0200

    categiria actual

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { PrismaClient } from '@prisma/client'
+import Layout from '../layout/Layout';
 
-export default function Home({categorias}) {
-  //console.log(categorias);
-  return <h1>Next.js</h1>;
-}
-
-export const getServerSideProps = async () => {
-  const prisma = new PrismaClient();
-
-  const categorias = await prisma.categoria.findMany();
-  //console.log(categorias)
-  return {
-    props: {
-      categorias,
-    },
-  };
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Inicio</h1>
+    </Layout>
+  );
 }
