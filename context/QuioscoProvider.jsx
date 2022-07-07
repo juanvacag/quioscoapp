58f8f2f69ae9e78bac9b9aaf3e233b1691849096
juanvacag <>
2022-07-07 09:30:31 +0200

    modal

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -7,6 +7,9 @@ const QuioscoProvider = ({children}) => {
 
     const [categorias, setCategorias] = useState([])
     const [categoriaActual, setCategoriaActual] = useState({})
+    const [producto, setProducto] = useState({})
+    const [modal, setModal] = useState(false)
+
 
     const obtenerCategorias = async () => {
         const {data} = await axios('/api/categorias')
@@ -21,18 +24,29 @@ const QuioscoProvider = ({children}) => {
         setCategoriaActual(categorias[0])
     },[categorias])
 
-    const handleClickCategoria = (id) => {
-        //console.log(id)
+    const handleClickCategoria = id => {
         const categoria = categorias.filter(cat => cat.id === id)
-        //console.log(categoria)
         setCategoriaActual(categoria[0])
     }
+
+    const handleSetProducto = producto => {
+        setProducto(producto)
+    }
+
+    const handleChangeModal = () => {
+        setModal(!modal)
+    }
+
     return(
         <QuioscoContext.Provider
             value={{
                 categorias,
                 categoriaActual,
-                handleClickCategoria
+                handleClickCategoria,
+                producto,
+                handleSetProducto,
+                modal,
+                handleChangeModal
             }}
         >
             {children}
--- layout/Layout.js
+++ layout/Layout.js
@@ -1,8 +1,26 @@
 import Head from 'next/head'
+import Modal from 'react-modal';
 import Sidebar from '../components/Sidebar'
+import ModalProducto from '../components/ModalProducto';
+import useQuiosco from '../hooks/useQuiosco';
+
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
+Modal.setAppElement('#__next');
 
 export default function Layout({children, pagina}) {
-    //console.log(pagina)
+
+    const {modal} = useQuiosco()
+
     return (
         <>
             <Head>
@@ -16,12 +34,17 @@ export default function Layout({children, pagina}) {
                 </aside>
 
                 <main className='md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll'>
-                    <div className='p-10'>
-                        {children}
-                    </div>    
-                        
+                    <div className='p-10'>{children}</div>    
                 </main>
             </div>
+            {Modal && (
+                <Modal
+                    isOpen={modal}
+                    style={customStyles}
+                >
+                    <ModalProducto />
+                </Modal>
+            )}
         </>
     );
   }
